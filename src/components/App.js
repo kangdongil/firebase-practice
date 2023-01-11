94c243d3e92b97ec6313fb4fbe39bc00d6e8ae33
kangdongil <akang8150@naver.com>
2023-01-11 16:37:57 +0900

    Delete and Edit Nweets

--- src/components/App.js
+++ src/components/App.js
@@ -3,29 +3,25 @@ import AppRouter from "router";
 import { authService } from "firebaseConfig";
 
 function App() {
-  const [ init, setInit ] = useState(false);
-  const [ isLoggedIn, setIsLoggedIn ] = useState(authService.currentUser);
-  const [ userObj, setUserObj ] = useState(null);
+  const [init, setInit] = useState(false);
+  // const [ isLoggedIn, setIsLoggedIn ] = useState(authService.currentUser);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
-      if (user){
-        setIsLoggedIn(true);
+      if (user) {
         setUserObj(user);
-      } else {
-        setIsLoggedIn(false);
       }
       setInit(true);
-    })
+    });
   }, []);
   return (
     <>
-    { init ? 
-      <AppRouter
-        isLoggedIn={isLoggedIn}
-        userObj={userObj}
-      /> : "Initializing..."
-    }
-    <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+      {init ? (
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+      ) : (
+        "Initializing..."
+      )}
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
     </>
   );
 }
--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,13 +1,56 @@
-import React from "react";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { dbService } from "firebaseConfig";
+import React, { useState } from "react";
 
-export default ({ nweetObj, isOwner }) => (
-  <div>
-    <h4>{nweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete Nweet</button>
-        <button>Edit NWeet</button>
-      </>
-    )}
-  </div>
-);
+export default ({ nweetObj, isOwner }) => {
+  const nweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this nweet?");
+    if (ok) {
+      await deleteDoc(nweetTextRef);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await updateDoc(nweetTextRef, {
+      text: newNweet,
+    });
+    setEditing(false);
+  };
+  const onChange = (event) => {
+    const { value } = event.target;
+    setNewNweet(value);
+  };
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your nweet"
+              value={newNweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
