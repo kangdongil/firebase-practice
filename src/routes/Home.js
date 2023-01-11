f5603b2fadb5b038f7905cf483a01a323dd7a4e2
kangdongil <akang8150@naver.com>
2023-01-11 16:18:44 +0900

    Check Nweet isOwner or not

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,61 +1,64 @@
-import { addDoc, collection, getDocs, onSnapshot, orderBy, query } from "firebase/firestore";
+import Nweet from "components/Nweet";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { dbService } from "firebaseConfig";
 import React, { useEffect, useState } from "react";
 
 export default ({ userObj }) => {
-    const [ nweet, setNweet ] = useState("");
-    const [ nweets, setNweets ] = useState([]);
-    useEffect(() => {
-        const q = query(
-            collection(dbService, "nweets"),
-            orderBy("createdAt", "desc")
-        );
-        onSnapshot(q, (snapshot) => {
-            const nweetArr = snapshot.docs.map((doc) => (
-                {
-                    id: doc.id,
-                    ...doc.data(),
-                }
-            ))
-            setNweets(nweetArr);
-        });
-    }, []);
-    const onSubmit = async(event) => {
-        event.preventDefault();
-        await addDoc(collection(dbService, "nweets"), {
-            text: nweet,
-            createdAt: Date.now(),
-            creatorId: userObj.uid,
-        });
-        setNweet("");
-    };
-    const onChange = (event) => {
-        const { value } = event.target;
-        setNweet(value);
-    }
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    type="text"
-                    placeholder="What's on your mind?"
-                    maxLength={120}
-                    value={nweet}
-                    onChange={onChange}
-                />
-                <input
-                    type="submit"
-                    value="Nweet"
-                />
-            </form>
-            <div>
-                {nweets.map((nweet) => (
-                    <div key={nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
-                    ))
-                }
-            </div>
-        </div>
-    )
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  useEffect(() => {
+    const q = query(
+      collection(dbService, "nweets"),
+      orderBy("createdAt", "desc")
+    );
+    onSnapshot(q, (snapshot) => {
+      const nweetArr = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArr);
+    });
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await addDoc(collection(dbService, "nweets"), {
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const { value } = event.target;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          value={nweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
