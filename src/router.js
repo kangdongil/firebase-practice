2674399b46fd37489c5b3a46718fd0e0f1dd944b
kangdongil <akang8150@naver.com>
2023-01-12 14:44:47 +0900

    Get my Own Nweets

--- src/router.js
+++ src/router.js
@@ -6,23 +6,23 @@ import Auth from "./routes/Auth";
 import Home from "./routes/Home";
 
 const AppRouter = ({ isLoggedIn, userObj }) => {
-    return (
-        <Router>
-            { isLoggedIn && <Navigation />}
-            <Routes>
-                <>
-                    { isLoggedIn ? (
-                        <>
-                            <Route path="/" element={<Home userObj={userObj} />} />
-                            <Route path="/profile" element={<Profile />} />
-                        </>
-                    ) : (
-                        <Route path="/" element={<Auth />} />
-                    )}
-                </>
-            </Routes>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Routes>
+        <>
+          {isLoggedIn ? (
+            <>
+              <Route path="/" element={<Home userObj={userObj} />} />
+              <Route path="/profile" element={<Profile userObj={userObj} />} />
+            </>
+          ) : (
+            <Route path="/" element={<Auth />} />
+          )}
+        </>
+      </Routes>
+    </Router>
+  );
+};
 
 export default AppRouter;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,17 +1,35 @@
-import { authService } from "firebaseConfig";
-import React from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { authService, dbService } from "firebaseConfig";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default () => {
-    const navigate = useNavigate();
-    const onLogOutClick = () => {
-        authService.signOut();
-        navigate(-1);
-        // navigate("/", { replace: true });
-    }
-    return (
-        <>
-            <button onClick={onLogOutClick}>Log Out</button>
-        </>
-    )
+export default ({ userObj }) => {
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/", { replace: true });
+  };
+  const getMyNweets = async () => {
+    let myNweetArr = [];
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      myNweetArr.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+    console.log(myNweetArr);
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 };
